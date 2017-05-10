7f42ec2497b14abf7a00b5bf7924cd83edecb533
Roman <kaboom.ua@gmail.com>
2017-05-10 19:01:57 +0300

    Update toggleArticle.js

--- src/decorators/toggleArticle.js
+++ src/decorators/toggleArticle.js
@@ -1,10 +1,12 @@
 //HOC === Higher Order Component === decorator
 import React, {Component as ReactComponent} from 'react'
 
+//DecoratedComponent я называл чтоб легче понять было. Лучше выбирай более значущее название
 export default (OriginalComponent) => class DecoratedComponent extends ReactComponent {
     constructor(props) {
         super()
         this.state = {
+        //Не привязывайся к названиям сущностей, вся суть декораторов в универсальности. Сделай openItemId
             openArticleId: ''
         }
     }
@@ -14,6 +16,7 @@ export default (OriginalComponent) => class DecoratedComponent extends ReactComp
     }
 
     toggleArticle = openArticleId => ev => {
+        //лучше undefined или null
         this.setState(this.state.openArticleId && (this.state.openArticleId === openArticleId) ? {openArticleId: ''} : {openArticleId})
     }
 }
