3a77ee7ce38e9863eb8f0c29bcfa249daf0a2272
Roman <kaboom.ua@gmail.com>
2017-05-18 11:23:40 +0300

    Update dateRange.js

--- src/reducer/dateRange.js
+++ src/reducer/dateRange.js
@@ -1,6 +1,7 @@
 import {ADD_DAY_TO_RANGE} from '../constants'
 import { DateUtils } from 'react-day-picker'
 
+//не дроби редюсеры слишком сильно: объедини селект и календарь в один
 export default (range = {from: null, to: null}, action) => {
     const {type, payload} = action
 
