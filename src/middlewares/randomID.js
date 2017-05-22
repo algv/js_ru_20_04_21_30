38121b3ead278fba7f9fcbb8851c8202d59424e0
Roman <kaboom.ua@gmail.com>
2017-05-22 14:58:19 +0300

    Update randomID.js

--- src/middlewares/randomID.js
+++ src/middlewares/randomID.js
@@ -4,6 +4,7 @@ export default store => next => action => {
     const { type, payload } = action
 
     switch (type) {
+        //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - не лучшее решение
         case ADD_COMMENT:
             action.payload.id = parseInt(Math.random().toString().substr(2, 5))
     }
